967b2aedbcfa1cef73604007f95a3ff5cfce5cd0
CloudLun <cloud830902@gmail.com>
2022-10-12 09:40:29 -0400

    heatmap height

--- heatmap.js
+++ heatmap.js
@@ -1,6 +1,6 @@
 const margin = { top: 30, right: 30, bottom: 30, left: 30 },
   heatWidth = 450 - margin.left - margin.right,
-  heatHeight = 108- margin.top - margin.bottom;
+  heatHeight = 111- margin.top - margin.bottom;
 
 const data = [
   "./data/熱力圖數據_人流分區差異_假日.csv",
